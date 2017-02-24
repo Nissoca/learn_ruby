1f7cb9ec532814f3ad604805c2a0aa900a853512
Nissoca <viniciusmello_2@hotmail.com>
2017-02-24 17:06:00 -0300

    Complete 05_book_title

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    @title = @title.split()
+    for i in 0..@title.length - 1
+      unless %w(a an and the in of).include? @title[i] and i != 0
+        @title[i] = @title[i].capitalize
+      end
+    end
+    @title = @title.join(" ")
+  end
+
 end
