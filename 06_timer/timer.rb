7d88d255d9a5898a49b1ce42f3fd72a8c7a9a566
Nissoca <viniciusmello_2@hotmail.com>
2017-02-24 17:46:10 -0300

    Complete 05_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,11 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0    
+  end
+  def time_string
+    time = Time.new(0) + @seconds
+    time.strftime("%H:%M:%S")
+  end
 end
